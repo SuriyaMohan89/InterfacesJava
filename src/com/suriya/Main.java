269e0f9242e321834b4e71da650ff1ddaeb598b7
SuriyaMohan89 <suriya.selu@gmail.com>
2018-08-15 14:43:28 -0700

    Initial commit

---
+++ src/com/suriya/Main.java
@@ -0,0 +1,69 @@
+package com.suriya;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+
+        Player Aadhi = new Player("Aadhu", 10,13);
+        System.out.println("toString");
+        saveObject(Aadhi);
+        Aadhi.setHitPoints(56);
+        Aadhi.setWeapon("Gun");
+        System.out.println(Aadhi);
+        Aadhi.setStrength(15);
+        System.out.println(Aadhi);
+        saveObject(Aadhi);
+        loadObject(Aadhi);
+        System.out.println(Aadhi);
+
+
+    }
+
+    public static ArrayList<String> readValues() {
+        ArrayList<String> values = new ArrayList<String>();
+
+        Scanner scanner = new Scanner(System.in);
+        boolean quit = false;
+        int index = 0;
+        System.out.println("Choose\n" +
+                "1 to enter a string\n" +
+                "0 to quit");
+
+        while (!quit) {
+            System.out.print("Choose an option: ");
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+            switch (choice) {
+                case 0:
+                    quit = true;
+                    break;
+                case 1:
+                    System.out.print("Enter a string: ");
+                    String stringInput = scanner.nextLine();
+                    values.add(index, stringInput);
+                    index++;
+                    break;
+            }
+        }
+        return values;
+    }
+
+    public static void saveObject(ISaveable objectToSave) {
+        for(int i=0; i<objectToSave.write().size(); i++) {
+            System.out.println("Saving " + objectToSave.write().get(i) + " to storage device");
+        }
+    }
+    public static void loadObject(ISaveable objectToLoad) {
+        ArrayList<String> values = readValues();
+        objectToLoad.read(values);
+    }
+
+}
+
+
+
+
